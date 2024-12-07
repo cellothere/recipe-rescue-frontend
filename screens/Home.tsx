0ac3767e347b5b7f3facc5d8b9019626c1f14ae3
cellothere <109959547+cellothere@users.noreply.github.com>
2024-12-07 16:52:48 -0500

    Some icons that need to be styled correctly

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Home: React.FC = () => {
   const navigation = useNavigation();
+  const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
@@ -12,14 +15,31 @@ const Home: React.FC = () => {
         style={styles.button}
         onPress={() => navigation.navigate('HomeTabs', { screen: 'Recipe Generator' })}
       >
+        <View style={styles.iconTextContainer}>
+      <MaterialIcon name="silverware-clean" size={30} color={isDarkMode ? '#fff' : '#000'} />
         <Text style={styles.buttonText}>Generate a Recipe</Text>
+        </View>
       </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('HomeTabs', { screen: 'Substitute' })}
+      >
+        <View style={styles.iconTextContainer}>
+        <Icon name="swap-horizontal" size={30} color={isDarkMode ? '#fff' : '#000'} />
+        <Text style={styles.buttonText}>Substitute Finder</Text>
+        </View>
+      </TouchableOpacity>
+
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('KitchenTabs', { screen: 'Recipes' })}
       >
-        <Text style={styles.buttonText}>View Saved Recipes</Text>
+        <View style={styles.iconTextContainer}>
+          <Icon name="heart-outline" size={30} color={isDarkMode ? '#fff' : '#000'} />
+          <Text style={styles.buttonText}>View Saved Recipes (7)</Text>
+        </View>
       </TouchableOpacity>
+
     </View>
   );
 };
@@ -48,6 +68,10 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 16,
     fontWeight: '600',
+  },
+  iconTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Vertically align items
   },
 });
 
